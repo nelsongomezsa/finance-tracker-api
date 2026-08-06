739620057b74192799cdf94464814a12f5a550fc
Nelson Gómez sanchez <nelsongomezsa@gmail.com>
2026-08-06 13:21:37 +0200

    feat: application entry point

---
+++ src/main/java/com/nelsongomez/financetracker/FinanceTrackerApiApplication.java
@@ -0,0 +1,12 @@
+package com.nelsongomez.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerApiApplication.class, args);
+    }
+}
